08475c569de2d24831f5217f09058d17b8537b0b
cmsk1 <consti93@gmx.net>
2022-01-27 12:51:45 +0100

    fix UUID generation

--- data-warehouse/src/main/java/de/htwberlin/schbuet/data_warehouse/data/main/WarehouseItem.java
+++ data-warehouse/src/main/java/de/htwberlin/schbuet/data_warehouse/data/main/WarehouseItem.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -17,7 +19,10 @@ import java.util.UUID;
 public class WarehouseItem {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(columnDefinition = "uniqueidentifier")
+    @Type(type = "org.hibernate.type.UUIDCharType")
     private UUID id;
 
     private UUID productId;
