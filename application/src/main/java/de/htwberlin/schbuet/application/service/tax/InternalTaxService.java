18811e039982d0311522f253727914fbbcf80910
stuntman11 <stuntman-projekts@gmx.de>
2022-01-29 16:46:48 +0100

    Added public systems to gateway config (KE-40)
    Includes minor routing adjustments

--- application/src/main/java/de/htwberlin/schbuet/application/service/tax/InternalTaxService.java
+++ application/src/main/java/de/htwberlin/schbuet/application/service/tax/InternalTaxService.java
@@ -1,4 +1,4 @@
-package de.htwberlin.schbuet.application.service;
+package de.htwberlin.schbuet.application.service.tax;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
@@ -7,13 +7,16 @@ import org.springframework.web.client.RestTemplate;
 import de.htwberlin.schbuet.application.data.request.RequestTax;
 
 @Service
-public class InternalCalculatorService implements CalculatorService {
+public class InternalTaxService implements TaxService {
 	private final RestTemplate rest;
 	
-	public InternalCalculatorService(RestTemplateBuilder restBuilder) {
-		this.rest = restBuilder.rootUri("http://localhost:3000").build();
+	public InternalTaxService() {
+		this.rest = new RestTemplateBuilder()
+				.rootUri("http://localhost:3000")
+				.build();
 	}
 	
+	@Override
 	public RequestTax getTaxForPrice(int priceInCents) {
 		String url = "/tax?priceInCents=" + priceInCents;
 		return rest.getForObject(url, RequestTax.class);
